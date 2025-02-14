45156e26006868fc45ef1b74723c28d52ec602a5
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-14 17:02:51 +0500

    page image is added to the screen with the properties

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -17,11 +17,18 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
-                
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
             } //: ZSTACK
-            .navigationTitle("Pinch")
+            .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
         } //: NAVIGATION
+        // Stack navigation style will avoid using the sidebar on iPad devices
         .navigationViewStyle(.stack)
     }
 }
