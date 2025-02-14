32e8e3683762ef8bd9c369b432fa12f3f2d6c716
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-14 17:08:38 +0500

    Animation added to show the image when the screen gets load

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - PROPERTY
+    @State private var isAnimating: Bool = false
     
     // MARK: - FUNCTION
     
@@ -24,9 +25,15 @@ struct ContentView: View {
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
         } //: NAVIGATION
         // Stack navigation style will avoid using the sidebar on iPad devices
         .navigationViewStyle(.stack)
