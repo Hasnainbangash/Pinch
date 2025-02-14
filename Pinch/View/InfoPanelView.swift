a7e5d67d2e189c530f8d0d214c4209124237d69e
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-14 17:50:34 +0500

    New file info panel view is generated along with th some preview setting and some comments are added to the project

---
+++ Pinch/View/InfoPanelView.swift
@@ -0,0 +1,25 @@
+//
+//  InfoPanelView.swift
+//  Pinch
+//
+//  Created by Elexoft on 14/02/2025.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var body: some View {
+        HStack {
+            // MARK: - HOTSPOT
+            
+            // MARK: - INFO PANEL
+        }
+    }
+}
+
+#Preview {
+    InfoPanelView()
+        .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
