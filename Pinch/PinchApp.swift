3cbbf3265fbdcdc436ed4b881f4d5af1197c953f
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-14 16:25:44 +0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Elexoft on 14/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
