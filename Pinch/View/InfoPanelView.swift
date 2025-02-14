91d1807919b73a7e4974b7a472db7f9ed245baed
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-14 18:04:01 +0500

    info panel data is entered

--- Pinch/View/InfoPanelView.swift
+++ Pinch/View/InfoPanelView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct InfoPanelView: View {
+    var scale: CGFloat
+    var offset: CGSize
+    
     var body: some View {
         HStack {
             // MARK: - HOTSPOT
@@ -19,12 +22,28 @@ struct InfoPanelView: View {
             Spacer()
             
             // MARK: - INFO PANEL
+            HStack(spacing: 2) {
+                Image(systemName: "arrow.up.left.and.arrow.down.right")
+                Text("\(scale)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.left.and.right")
+                Text("\(offset.width)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.up.and.down")
+                Text("\(offset.height)")
+                
+                Spacer()
+            }
         }
     }
 }
 
 #Preview {
-    InfoPanelView()
+    InfoPanelView(scale: 1, offset: .zero)
         .preferredColorScheme(.dark)
         .previewLayout(.sizeThatFits)
         .padding()
