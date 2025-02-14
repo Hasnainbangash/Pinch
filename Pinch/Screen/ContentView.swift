e8c665deebe662013b9a79155473a5cad4aa332b
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-14 16:54:08 +0500

    Navigation view is added along wth the some comments

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                
+            } //: ZSTACK
+            .navigationTitle("Pinch")
+            .navigationBarTitleDisplayMode(.inline)
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
