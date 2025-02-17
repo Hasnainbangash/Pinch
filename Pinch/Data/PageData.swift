c961ce5c995b95ab733fbe60a3e3deea3b7e8d44
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-17 13:02:19 +0500

    Page model and page data is created along with the file

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Elexoft on 17/02/2025.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
---
+++ Pinch/Model/PageModel.swift
@@ -0,0 +1,13 @@
+//
+//  PageModel.swift
+//  Pinch
+//
+//  Created by Elexoft on 17/02/2025.
+//
+
+import Foundation
+
+struct Page: Identifiable {
+    let id: Int
+    let imageName: String
+}
